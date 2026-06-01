1ec328b09473dae687fde3626c6b0a46f216ff18
Paul B. Krause <paul.krause@oracle.com>
2026-06-01 12:35:08 -0400

    More skeleton

--- src/bin/table-tui.rs
+++ src/bin/table-tui.rs
@@ -2,8 +2,20 @@
 // Test printing a layout
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
+use std::{io, thread, time::Duration};
+
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
 use seahaven::{deck::Deck, game::Table};
 
@@ -16,7 +28,29 @@ pub fn main() {
 
     let t = Table::from(&mut d);
 
-    let stdout = io::stdout();
+    // Set up terminal
+    enable_raw_mode().unwrap();
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture).unwrap();
     let backend = CrosstermBackend::new(stdout);
-    let mut term = Terminal::new(backend);
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    }).unwrap();
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode().unwrap();
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    ).unwrap();
+    terminal.show_cursor().unwrap();
 }
